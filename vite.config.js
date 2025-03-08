230be34b2d1f364fde60c4a74ebb964983050a0c
Denise Wayne <waynedenise.n@gmail.com>
2025-03-07 19:36:12 -0800

    add backend api

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      '/data': {
+        target: 'https://lahelpbackend-2.azurewebsites.net',
+        changeOrigin: true,
+      },
+    }
+  }
 })
