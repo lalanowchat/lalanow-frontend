d86e70d3769f18a4cd19b4c295c0e576c31847cf
Denise Wayne <waynedenise.n@gmail.com>
2025-03-23 10:41:25 -0700

    change direct link to malan instead

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
           </Link>
           {/* Want to Help Button */}
           <Link
-            to="/want-to-help"
+            to="https://www.mutualaid.la/resources"
             className="flex-1 p-4 text-lg md:text-xl bg-green-500 text-white rounded-lg hover:bg-green-600 transition-colors text-center"
           >
             {t('header.wantToHelp')}
